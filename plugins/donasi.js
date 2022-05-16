5be40fbdc6e3c2c85b59b3cbc20972b070a13166
Diluc <91557468+Dilucal@users.noreply.github.com>
2022-05-16 11:30:05 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,20 @@
 let handler =  m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085713964963]
-│ • Telkomsel [081319944687]
+│ • XL [087823406145]
+│ • Telkomsel [082283541084]
 ╰────
 
-╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/BochilGaming
-│ • Gopay [085713964963]
+╭─「 Donasi • Cip Higgs domino 」
+│ • ID 1   [ 312501015 ]
+│ • ID 2   [ 305037960 ]
+╰────
+╭─「 Sosial Media Owner 」
+│ • INSTAGRAM 
+| × https://instagram.com/armnd.at
+│ • OWNER
+| × wa.me/6287823406145
+| • GITHUB
+| × https://github.com/Dilucal
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
